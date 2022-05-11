15ca9d18f91beffe281257bd2d0e4f676e8fb6ef
Bhuvana1819 <104617608+Bhuvana1819@users.noreply.github.com>
2022-05-11 23:51:26 +0530

    Add files via upload

---
+++ SQL/Activities/sqlscript (1).sql
@@ -0,0 +1,118 @@
+REM   Script: ACTIVITY 1 & ACTIVITY 2 SQL
+REM   ACTIVITY 1 & ACTIVITY 2 SQL
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES (5003, 'Lauson Hen', 'San Jose', 12);
+
+select * from salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES (5002, 'Nail Knite', 'Paris', 13);
+
+select * from salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES (5005, 'Pit Alex', 'London', 11);
+
+select * from salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES (5006, 'McLyon', 'Paris', 14);
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES (5007, 'Paul Adam', 'Rome', 13);
+
+select * from salesman;
+
+select * from salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES (5001, 'James Hoog', 'New York', 15);
+
+select * from salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+desc salesman
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES (5001, 'James Hoog', 'New York', 15);
+
+INSERT INTO salesman VALUES (5007, 'Bhuvana', 'Chennai', 25) 
+;
+
+select * from salesman;
+
