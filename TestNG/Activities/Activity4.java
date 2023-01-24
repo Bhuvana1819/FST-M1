ea5226c5e9195c5aaaed21a4c03d0697da246913
Bhuvana1819 <104617608+Bhuvana1819@users.noreply.github.com>
2023-01-25 01:31:20 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4.java
@@ -0,0 +1,35 @@
+package Activity;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Activity4
+{
+    WebDriver driver;
+    @BeforeMethod
+    public void beforeMethod()
+    {
+        driver = new FirefoxDriver();
+        driver.get("https://www.training-support.net");
+    }
+    @Test
+    public void exampleTestcase()
+    {
+        System.out.println("Title of the book is : " + driver.getTitle());
+        Assert.assertEquals("Training Support" , driver.getTitle());
+        driver.findElement(By.id("about-link")).click();
+        System.out.println("Title of the book is : " + driver.getTitle());
+        Assert.assertEquals("About Training Support" , driver.getTitle());
+    }
+
+    @AfterMethod
+    public void afterMethod()
+    {
+        driver.quit();
+    }
+}
